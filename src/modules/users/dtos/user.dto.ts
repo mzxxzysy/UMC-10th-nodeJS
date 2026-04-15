048f8c37c86b4bb8e3598267eb8711537df0953d
mzxxzysy <jsyjj21@gmail.com>
2026-04-15 14:47:41 +0900

    ✨ Feat: 회원가입 api 구현 (#3)

---
+++ src/modules/users/dtos/user.dto.ts
@@ -0,0 +1,52 @@
+// 1. 회원가입 요청 데이터의 설계도를 만듭니다.
+export interface UserSignUpRequest {
+  email: string;
+  name: string;
+  gender: string;
+  birth: string;
+  address?: string; // ?가 붙으면 '없을 수도 있음(선택)'이라는 뜻이에요!
+  detailAddress?: string;
+  phoneNumber: string;
+  preferences: number[];
+}
+
+// 2. 요청받은 데이터를 우리 시스템에 맞는 데이터로 변환해주는 함수입니다.
+export const bodyToUser = (body: UserSignUpRequest) => {
+  return {
+    email: body.email, //필수
+    name: body.name, // 필수
+    gender: body.gender, // 필수
+    birth: body.birth, // 필수
+    address: body.address || "", //선택
+    detailAddress: body.detailAddress || "", //선택
+    phoneNumber: body.phoneNumber, //필수
+    preferences: body.preferences, // 필수
+  };
+};
+
+//responseFromUser
+export interface UserSignUpResponse {
+  userId: number;
+  email: string;
+  name: string;
+  gender: string;
+  birth: Date;
+  address?: string;
+  detailAddress?: string;
+  phoneNumber: string;
+  preferences: number[];
+}
+
+export const responseFromUser = ({ user, preferences }: { user: any; preferences: any[] }): UserSignUpResponse => {
+  return {
+    userId: user.userId,
+    email: user.email,
+    name: user.name,
+    gender: user.gender,
+    birth: user.birth,
+    address: user.address || "",
+    detailAddress: user.detailAddress || "",
+    phoneNumber: user.phoneNumber,
+    preferences: preferences.map((p) => p.preferenceId),
+  };
+};
