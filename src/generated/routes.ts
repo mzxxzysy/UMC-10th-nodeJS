7f6748da7c2dfc25b026e9ed689b4f14b8fa5ff1
mzxxzysy <jsyjj21@gmail.com>
2026-05-25 13:42:25 +0900

    ✨ Feat: 스웨거 설정 (#9)

--- src/generated/routes.ts
+++ src/generated/routes.ts
@@ -39,6 +39,16 @@ const models: TsoaRoute.Models = {
         "additionalProperties": false,
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "ApiResponse_null_": {
+        "dataType": "refObject",
+        "properties": {
+            "resultType": {"dataType":"enum","enums":["SUCCESS"],"required":true},
+            "error": {"dataType":"enum","enums":[null],"required":true},
+            "data": {"dataType":"enum","enums":[null],"required":true},
+        },
+        "additionalProperties": false,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     "UserSignUpRequest": {
         "dataType": "refObject",
         "properties": {
--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Middlewares, Post, Request, Res, Route, Tags } from "tsoa";
+import { Body, Controller, Get, Middlewares, Post, Request, Res, Route, Tags, Response } from "tsoa";
 import { UserSignUpRequest, UserSignUpResponse } from "../dtos/user.dto.js";
 import { userSignUp } from "../services/user.service.js";
 import { ApiResponse, success } from "../../../common/responses/response.js";
@@ -8,7 +8,13 @@ import { Request as ExpressRequest } from "express";
 @Route("users") // 라우트 경로
 @Tags("Users") // Swagger 태그
 export class UserController extends Controller {
+  /**
+   * 회원가입 API
+   * @summary 회원가입을 처리하는 엔드포인트입니다.
+   */
   @Post("signup") // 엔드포인드 정의
+  @Response<ApiResponse<UserSignUpResponse>>(200, "회원가입 성공")
+  @Response<ApiResponse<null>>(400, "중복된 이메일 에러")
   public async handleUserSignUp(@Body() body: UserSignUpRequest): Promise<ApiResponse<UserSignUpResponse>> {
     console.log("회원가입을 요청했습니다!");
     console.log("body:", body);
